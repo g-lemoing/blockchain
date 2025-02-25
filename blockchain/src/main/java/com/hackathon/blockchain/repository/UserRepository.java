f7cd25c344df8986fe592083482cb151f373b2a2
g-lemoing <le.moing.gwenael@gmail.com>
2025-02-25 19:15:11 +0100

    UserRepository> create

---
+++ blockchain/src/main/java/com/hackathon/blockchain/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.hackathon.blockchain.repository;
+
+import com.hackathon.blockchain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+}
