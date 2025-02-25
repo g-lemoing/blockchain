281eb096620ac44c5cf0e4ac6959ff6d34a0c709
g-lemoing <le.moing.gwenael@gmail.com>
2025-02-25 19:13:58 +0100

    LoginResponseDto, LoginUserDto, RegisterUserDto> create

---
+++ blockchain/src/main/java/com/hackathon/blockchain/dto/LoginResponseDto.java
@@ -0,0 +1,27 @@
+package com.hackathon.blockchain.dto;
+
+public class LoginResponseDto {
+    private String jwtToken;
+    private String username;
+
+    public LoginResponseDto(String jwtToken, String username) {
+        this.jwtToken = jwtToken;
+        this.username = username;
+    }
+
+    public String getJwtToken() {
+        return jwtToken;
+    }
+
+    public void setJwtToken(String jwtToken) {
+        this.jwtToken = jwtToken;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
