224c70d85d5f74b717fd2d0121977409843b8f5b
g-lemoing <le.moing.gwenael@gmail.com>
2025-02-26 16:41:57 +0100

    LoginResponseDto> replace previous class with record

--- blockchain/src/main/java/com/hackathon/blockchain/dto/LoginResponseDto.java
+++ blockchain/src/main/java/com/hackathon/blockchain/dto/LoginResponseDto.java
@@ -1,27 +1,5 @@
 package com.hackathon.blockchain.dto;
 
-public class LoginResponseDto {
-    private String jwtToken;
-    private String username;
+public record LoginResponseDto <T> (int httpStatus, String message, T data){
 
-    public LoginResponseDto(String jwtToken, String username) {
-        this.jwtToken = jwtToken;
-        this.username = username;
-    }
-
-    public String getJwtToken() {
-        return jwtToken;
-    }
-
-    public void setJwtToken(String jwtToken) {
-        this.jwtToken = jwtToken;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
 }
