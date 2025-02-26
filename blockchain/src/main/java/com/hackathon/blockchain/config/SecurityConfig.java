b1df7811afdd4e57c1479cac5cfa5b637e9e9d49
g-lemoing <le.moing.gwenael@gmail.com>
2025-02-26 16:43:07 +0100

    SecurityConfig> modify securityFilterChain()

--- blockchain/src/main/java/com/hackathon/blockchain/config/SecurityConfig.java
+++ blockchain/src/main/java/com/hackathon/blockchain/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.hackathon.blockchain.config;
 
 import com.hackathon.blockchain.repository.UserRepository;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,16 +10,23 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.RequestCacheConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
     private final UserRepository userRepository;
 
@@ -30,10 +38,14 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         final String[] WHITE_LIST_URL = {"/actuator/**", "/v3/api-docs/**", "/swagger-resources",
                 "/swagger-resources/**", "/swagger-ui/**", "/swagger-ui.html", "/auth/register",
-                "/auth/login","/openapi.yaml"};
+                "/auth/login", "/auth/logout", "/openapi.yaml"};
 
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .authenticationProvider(authenticationProvider())
+                .securityContext(context ->
+                        context.securityContextRepository(securityContextRepository()))
+                .requestCache(RequestCacheConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(
                         SessionCreationPolicy.IF_REQUIRED))
                 .cors(Customizer.withDefaults())
@@ -47,11 +59,9 @@ public class SecurityConfig {
                         })
                 ))
                 .logout(logout -> logout.logoutUrl("/auth/logout")
-                        .logoutSuccessHandler((request, response, authentication) -> {
-                            response.setStatus(HttpServletResponse.SC_OK);
-                            response.getWriter().write("{\"message\": \"Logged out successfully\"}");
-                        })
+                        .logoutSuccessHandler(logoutSuccessHandler())
                         .invalidateHttpSession(true)
+                        .clearAuthentication(true)
                         .deleteCookies("JSESSIONID"));
 
         return http.build();
@@ -79,5 +89,17 @@ public class SecurityConfig {
     UserDetailsService userDetailsService() {
         return username -> userRepository.findByUsername(username).stream().findFirst()
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    public SecurityContextRepository securityContextRepository() {
+        return new HttpSessionSecurityContextRepository();
+    }
+
+    public LogoutSuccessHandler logoutSuccessHandler(){
+        return (request, response, authentication) -> {
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.getWriter().write("{\"message\": \"Logged out successfully\"}");
+        };
     }
 }
