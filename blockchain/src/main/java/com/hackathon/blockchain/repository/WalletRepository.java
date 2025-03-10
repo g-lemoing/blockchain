29bad7fc1b23a407421ad8b0b8620f8e928f8f17
g-lemoing <le.moing.gwenael@gmail.com>
2025-03-10 18:44:20 +0100

    WalletRepository> first commit

---
+++ blockchain/src/main/java/com/hackathon/blockchain/repository/WalletRepository.java
@@ -0,0 +1,11 @@
+package com.hackathon.blockchain.repository;
+
+import com.hackathon.blockchain.model.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface WalletRepository extends JpaRepository<Wallet, Long> {
+    Optional<Wallet> findByUserId(Long userId);
+    Optional<Wallet> findByAddress(String address);
+}
