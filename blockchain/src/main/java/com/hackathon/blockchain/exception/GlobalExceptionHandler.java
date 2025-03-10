3a8e2d7f6b84e183eed043379cfae41effd0f086
g-lemoing <le.moing.gwenael@gmail.com>
2025-03-10 18:38:45 +0100

    GlobalExceptionHandler> add IO, NoSuchAlgorithm, Interrupted, AssetNotFound

--- blockchain/src/main/java/com/hackathon/blockchain/exception/GlobalExceptionHandler.java
+++ blockchain/src/main/java/com/hackathon/blockchain/exception/GlobalExceptionHandler.java
@@ -6,6 +6,9 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     ProblemDetail problemDetail = null;
@@ -14,6 +17,34 @@ public class GlobalExceptionHandler {
     public ProblemDetail handleBadCredentialsException(BadCredentialsException e){
         problemDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(401), e.getMessage());
         problemDetail.setProperty("description", "Invalid credentials");
+        return problemDetail;
+    }
+
+    @ExceptionHandler(IOException.class)
+    public ProblemDetail handleIOException(IOException e){
+        problemDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(404), e.getMessage());
+        problemDetail.setProperty("description", "A problem occured when storing / retrieving the key.");
+        return problemDetail;
+    }
+
+    @ExceptionHandler(NoSuchAlgorithmException.class)
+    public ProblemDetail handleNoSuchAlgorithmException(NoSuchAlgorithmException e){
+        problemDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(500), e.getMessage());
+        problemDetail.setProperty("description", "A problem occured when generating the key.");
+        return problemDetail;
+    }
+
+    @ExceptionHandler(InterruptedException.class)
+    public ProblemDetail handleIOException(InterruptedException e){
+        problemDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(500), e.getMessage());
+        problemDetail.setProperty("description", "A problem occured when retrieving the market prices.");
+        return problemDetail;
+    }
+
+    @ExceptionHandler(AssetNotFoundException.class)
+    public ProblemDetail handleAssetNotFoundException(AssetNotFoundException e){
+        problemDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(400), e.getMessage());
+        problemDetail.setProperty("{\"message", "❌ Asset not found or price unavailable: " + e.getLocalizedMessage() + "}");
         return problemDetail;
     }
 }
