4064337374a4b90be83d8f81f96958994d5b7d99
g-lemoing <le.moing.gwenael@gmail.com>
2025-03-10 18:36:02 +0100

    Asset> create

---
+++ blockchain/src/main/java/com/hackathon/blockchain/model/Asset.java
@@ -0,0 +1,73 @@
+package com.hackathon.blockchain.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "asset")
+public class Asset {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "purchase_price", nullable = false)
+    private double purchasePrice;
+    @Column(nullable = false)
+    private Long quantity;
+    @Column(nullable = false)
+    private String symbol;
+
+    @ManyToOne
+    @JoinColumn(name = "wallet_id", nullable = false)
+    private Wallet wallet;
+
+    public Asset(Long id, String symbol, double purchasePrice, long quantity, Wallet wallet) {
+        this.id = id;
+        this.purchasePrice = purchasePrice;
+        this.quantity = quantity;
+        this.symbol = symbol;
+        this.wallet = wallet;
+    }
+
+    public Asset() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(double purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(long quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public Wallet getWallet() {
+        return wallet;
+    }
+
+    public void setWallet(Wallet wallet) {
+        this.wallet = wallet;
+    }
+
+}
