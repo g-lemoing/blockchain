e49232bdd56d3100e0dead7409fac4435c98a26f
g-lemoing <le.moing.gwenael@gmail.com>
2025-03-12 17:30:12 +0100

    SignatureUtil> first commit

---
+++ blockchain/src/main/java/com/hackathon/blockchain/utils/SignatureUtil.java
@@ -0,0 +1,9 @@
+package com.hackathon.blockchain.utils;
+
+import java.security.PublicKey;
+
+public class SignatureUtil {
+    public static boolean verifySignature(String data, String digitalSignature, PublicKey publicKey){
+        return false;
+    }
+}
