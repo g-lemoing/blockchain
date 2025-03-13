a4123750d962e8a8faac76803c94b04c06da7846
g-lemoing <le.moing.gwenael@gmail.com>
2025-03-13 19:37:46 +0100

    NoPendingTransactionsException> create

---
+++ blockchain/src/main/java/com/hackathon/blockchain/exception/NoPendingTransactionsException.java
@@ -0,0 +1,7 @@
+package com.hackathon.blockchain.exception;
+
+public class NoPendingTransactionsException extends RuntimeException {
+  public NoPendingTransactionsException() {
+    super();
+  }
+}
