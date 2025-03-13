71922c23bca2a8d53e626f629d2e5d49556dd0c4
g-lemoing <le.moing.gwenael@gmail.com>
2025-03-13 19:34:49 +0100

    BlockchainController> create

---
+++ blockchain/src/main/java/com/hackathon/blockchain/controller/BlockchainController.java
@@ -0,0 +1,37 @@
+package com.hackathon.blockchain.controller;
+
+import com.hackathon.blockchain.model.Block;
+import com.hackathon.blockchain.service.BlockchainService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.NoSuchAlgorithmException;
+
+@RestController
+@RequestMapping("/blockchain")
+public class BlockchainController {
+    @Autowired
+    private BlockchainService blockchainService;
+
+    @PostMapping("/mine")
+    public ResponseEntity<String> mineTransactions() throws NoSuchAlgorithmException {
+        String hash= blockchainService.mineBlock();
+        return ResponseEntity.ok(hash);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Block[]> getBlocks(){
+        return ResponseEntity.ok(blockchainService.getBlocks());
+    }
+
+    @GetMapping("/validate")
+    public ResponseEntity<String> validateBlockchain() throws NoSuchAlgorithmException {
+        boolean isValid = blockchainService.isChainValid();
+        String message = "{\"message\": \"Blockchain valid: " + String.valueOf(isValid) + "\"}";
+        return ResponseEntity.ok(message);
+    }
+}
