2c1266e9d46df05d641c12060c66beb5d4bfe470
g-lemoing <le.moing.gwenael@gmail.com>
2025-03-13 19:36:54 +0100

    BlockRepository> add findByBlockIndex()

--- blockchain/src/main/java/com/hackathon/blockchain/repository/BlockRepository.java
+++ blockchain/src/main/java/com/hackathon/blockchain/repository/BlockRepository.java
@@ -3,5 +3,8 @@ package com.hackathon.blockchain.repository;
 import com.hackathon.blockchain.model.Block;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BlockRepository extends JpaRepository<Block, Long> {
+    Optional<Block> findByBlockIndex(Long blockIndex);
 }
