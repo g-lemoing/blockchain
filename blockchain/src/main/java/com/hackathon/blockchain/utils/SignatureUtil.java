2e19a98d736f040338624c7627638207b1cc792a
g-lemoing <le.moing.gwenael@gmail.com>
2025-03-13 12:39:27 +0100

    SignatureUtil> implement verifySignature()

--- blockchain/src/main/java/com/hackathon/blockchain/utils/SignatureUtil.java
+++ blockchain/src/main/java/com/hackathon/blockchain/utils/SignatureUtil.java
@@ -1,9 +1,33 @@
 package com.hackathon.blockchain.utils;
 
-import java.security.PublicKey;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
+import java.util.Base64;
 
 public class SignatureUtil {
-    public static boolean verifySignature(String data, String digitalSignature, PublicKey publicKey){
-        return false;
+    public static boolean verifySignature(String data, String digitalSignature, PublicKey publicKey)
+            throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+
+        byte[] messageBytes = getHashedMessage(data);
+        Signature verifSign = Signature.getInstance("SHA256withRSA");
+        verifSign.initVerify(publicKey);
+        verifSign.update(messageBytes);
+        return verifSign.verify(Base64.getDecoder().decode(digitalSignature));
+    }
+
+    public static byte[] getHashedMessage(String stringContract)
+            throws NoSuchAlgorithmException {
+        byte[] messageBytes = stringContract.getBytes(StandardCharsets.UTF_8);
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        return digest.digest(messageBytes);
+    }
+
+    public static String getSignature(byte[] hashedMessage, PrivateKey privateKey)
+            throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+        Signature signature = Signature.getInstance("SHA256withRSA");
+        signature.initSign(privateKey);
+        signature.update(hashedMessage);
+        byte[] digitalSignature = signature.sign();
+        return Base64.getEncoder().encodeToString(digitalSignature);
     }
 }
