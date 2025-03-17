d4882993b4f8c0aa404be3b4472ef8286a20c05f
g-lemoing <le.moing.gwenael@gmail.com>
2025-03-17 14:05:57 +0100

    BlockchainApplication > remove imports

--- blockchain/src/main/java/com/hackathon/blockchain/BlockchainApplication.java
+++ blockchain/src/main/java/com/hackathon/blockchain/BlockchainApplication.java
@@ -1,10 +1,7 @@
 package com.hackathon.blockchain;
 
-//import com.hackathon.blockchain.controller.DashboardController;
-import com.hackathon.blockchain.service.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.freemarker.FreeMarkerTemplateAvailabilityProvider;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "com.hackathon.blockchain")
