cdd8a96f693c4c24eb8670b2ee2f1f321cff8f4c
g-lemoing <le.moing.gwenael@gmail.com>
2025-03-17 11:09:42 +0100

    WalletRepository > add findByAddressStartsWith()

--- blockchain/src/main/java/com/hackathon/blockchain/repository/WalletRepository.java
+++ blockchain/src/main/java/com/hackathon/blockchain/repository/WalletRepository.java
@@ -3,9 +3,11 @@ package com.hackathon.blockchain.repository;
 import com.hackathon.blockchain.model.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
     Optional<Wallet> findByUserId(Long userId);
     Optional<Wallet> findByAddress(String address);
+    List<Wallet> findByAddressStartsWith(String address);
 }
